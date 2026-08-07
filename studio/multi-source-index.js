e2c0ab9646c4b6844bd9f4ecad5972a9307854f3
IMONsergey <jabba.person@gmail.com>
2026-08-07 14:15:39 +0300

    refactor: include motion craft source batch 4 in index

--- studio/multi-source-index.js
+++ studio/multi-source-index.js
@@ -1,12 +1,14 @@
 import { MULTI_SOURCE_CASES as BATCH_1 } from './multi-source-cases.js';
 import { MULTI_SOURCE_CASES_BATCH_2 as BATCH_2 } from './multi-source-cases-batch2.js';
 import { MULTI_SOURCE_CASES_BATCH_3 as BATCH_3 } from './multi-source-cases-batch3.js';
+import { MULTI_SOURCE_CASES_BATCH_4 as BATCH_4 } from './multi-source-cases-batch4.js';
 
-export const MULTI_SOURCE_CASES = [...BATCH_1, ...BATCH_2, ...BATCH_3];
+export const MULTI_SOURCE_CASES = [...BATCH_1, ...BATCH_2, ...BATCH_3, ...BATCH_4];
 
 export const MULTI_SOURCE_BATCH_STATS = {
   batch1: BATCH_1.length,
   batch2: BATCH_2.length,
   batch3: BATCH_3.length,
-  total: BATCH_1.length + BATCH_2.length + BATCH_3.length
+  batch4: BATCH_4.length,
+  total: BATCH_1.length + BATCH_2.length + BATCH_3.length + BATCH_4.length
 };
