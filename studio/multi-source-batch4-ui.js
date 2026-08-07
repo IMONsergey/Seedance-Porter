bbc6c547e0dae804013186c9d33d9d25886174f4
IMONsergey <jabba.person@gmail.com>
2026-08-07 14:15:08 +0300

    feat: mount motion craft source expansion batch 4

---
+++ studio/multi-source-batch4-ui.js
@@ -0,0 +1,4 @@
+import { MULTI_SOURCE_CASES_BATCH_4 } from './multi-source-cases-batch4.js';
+import { mountCaseBatch } from './multi-source-batch-runtime.js';
+
+mountCaseBatch(MULTI_SOURCE_CASES_BATCH_4, 'batch4');
